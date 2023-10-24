a3089aba04bd0813e2e6b5dd8945d890cb1d68fd
nattkarn <armnattkan@gmail.com>
2023-10-24 11:06:22 +0700

    change root path to swagger

--- src/main/java/com/testing/api/backend/api/HomeApi.java
+++ src/main/java/com/testing/api/backend/api/HomeApi.java
@@ -1,13 +1,16 @@
 package com.testing.api.backend.api;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 
+@Controller
 public class HomeApi {
 
     @GetMapping("/")
     public String root() {
-        return "index";
+        return "redirect:/swagger-ui/index.html#/";
     }
 
 
